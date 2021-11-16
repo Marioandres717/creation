ba11cab00911d344b7b393000a48e40ffa14c076
Mario Andres <marioarendon2@gmail.com>
2021-11-15 19:03:58 -0600

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.use(cookieParser());
   await app.listen(3000);
 
